2ed9afd70f946f1891b29c515bb3a0111385546e
Lemonnnnnnnnn <673762174@qq.com>
2022-03-10 10:03:01 +0800

    基础设计

--- app/src/main/java/com/yizhixionga/vcodetextview/MainActivity.java
+++ app/src/main/java/com/yizhixionga/vcodetextview/MainActivity.java
@@ -3,6 +3,9 @@ package com.yizhixionga.vcodetextview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.yizhixionga.textview.VCodeTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VCodeTextView vCodeTextView =new VCodeTextView(MainActivity.this);
+        vCodeTextView.setVCodeCompleteListener(new VCodeTextView.VCodeCompleteListener() {
+            @Override
+            public void VCodeComplete(String code) {
+                Toast.makeText(MainActivity.this,code,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
