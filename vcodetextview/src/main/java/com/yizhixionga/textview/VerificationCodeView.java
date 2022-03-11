b986d8de18928d9240601b1bf0384585f5eb29dd
Lemonnnnnnnnn <673762174@qq.com>
2022-03-11 09:19:55 +0800

    格式化代码

--- vcodetextview/src/main/java/com/yizhixionga/textview/VerificationCodeView.java
+++ vcodetextview/src/main/java/com/yizhixionga/textview/VerificationCodeView.java
@@ -62,100 +62,100 @@ public class VerificationCodeView extends RelativeLayout {
         this(context, attrs, 0);
     }
 
-public VerificationCodeView(Context context, AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
+    public VerificationCodeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
 
-    //获取自定义样式的属性
-    TypedArray typedArray =context.obtainStyledAttributes(attrs,R.styleable.VCodeTextView);
-    vCodeLength = typedArray.getInteger(R.styleable.VCodeTextView_vCodeDataLength,6);
-    tvBgNormal = typedArray.getResourceId(R.styleable.VCodeTextView_vCodeBackgroundNormal,R.drawable.bg_normal);
-    tvBgFocus = typedArray.getResourceId(R.styleable.VCodeTextView_vCodeBackgroundFocus,R.drawable.bg_focus);
-    tvMarginRight = typedArray.getDimensionPixelSize(R.styleable.VCodeTextView_vCodeMargin, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,10,getResources().getDisplayMetrics()));
-    tvWidth = typedArray.getDimensionPixelSize(R.styleable.VCodeTextView_vCodeWidth, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,45,getResources().getDisplayMetrics()));
-    tvHeight = typedArray.getDimensionPixelSize(R.styleable.VCodeTextView_vCodeHeight, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,45,getResources().getDisplayMetrics()));
-    tvTextSize = typedArray.getDimensionPixelSize(R.styleable.VCodeTextView_vCodeTextSize, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,20,getResources().getDisplayMetrics()));
-    tvTextColor = typedArray.getColor(R.styleable.VCodeTextView_vCodeTextColor, Color.BLACK);
-    isBold = typedArray.getBoolean(R.styleable.VCodeTextView_vCodeTextBold,false);
-    //用完回收
-    typedArray.recycle();
-    init();
-}
+        //获取自定义样式的属性
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.VCodeTextView);
+        vCodeLength = typedArray.getInteger(R.styleable.VCodeTextView_vCodeDataLength, 6);
+        tvBgNormal = typedArray.getResourceId(R.styleable.VCodeTextView_vCodeBackgroundNormal, R.drawable.bg_normal);
+        tvBgFocus = typedArray.getResourceId(R.styleable.VCodeTextView_vCodeBackgroundFocus, R.drawable.bg_focus);
+        tvMarginRight = typedArray.getDimensionPixelSize(R.styleable.VCodeTextView_vCodeMargin, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics()));
+        tvWidth = typedArray.getDimensionPixelSize(R.styleable.VCodeTextView_vCodeWidth, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 45, getResources().getDisplayMetrics()));
+        tvHeight = typedArray.getDimensionPixelSize(R.styleable.VCodeTextView_vCodeHeight, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 45, getResources().getDisplayMetrics()));
+        tvTextSize = typedArray.getDimensionPixelSize(R.styleable.VCodeTextView_vCodeTextSize, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 20, getResources().getDisplayMetrics()));
+        tvTextColor = typedArray.getColor(R.styleable.VCodeTextView_vCodeTextColor, Color.BLACK);
+        isBold = typedArray.getBoolean(R.styleable.VCodeTextView_vCodeTextBold, false);
+        //用完回收
+        typedArray.recycle();
+        init();
+    }
 
 
-private void init() {
-    initTextView();
-    initEditText();
-    tvSetFocus(0);
-}
+    private void init() {
+        initTextView();
+        initEditText();
+        tvSetFocus(0);
+    }
 
-/**
- * 输入框和父布局一样大，但字体大小0，看不见的
- */
-private void initEditText() {
-    editText = new EditText(getContext());
-    addView(editText);
-    LayoutParams layoutParams = (LayoutParams) editText.getLayoutParams();
-    layoutParams.width = layoutParams.MATCH_PARENT;
-    layoutParams.height = tvHeight;
-    editText.setLayoutParams(layoutParams);
+    /**
+     * 输入框和父布局一样大，但字体大小0，看不见的
+     */
+    private void initEditText() {
+        editText = new EditText(getContext());
+        addView(editText);
+        LayoutParams layoutParams = (LayoutParams) editText.getLayoutParams();
+        layoutParams.width = layoutParams.MATCH_PARENT;
+        layoutParams.height = tvHeight;
+        editText.setLayoutParams(layoutParams);
 
-    //防止横盘小键盘全屏显示
-    editText.setImeOptions(EditorInfo.IME_FLAG_NO_FULLSCREEN);
-    //隐藏光标
-    editText.setCursorVisible(false);
-    //最大输入长度
-    editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(vCodeLength)});
-    //输入类型为数字
-    editText.setInputType(InputType.TYPE_CLASS_NUMBER);
-    editText.setTextSize(0);
-    editText.setBackgroundResource(0);
+        //防止横盘小键盘全屏显示
+        editText.setImeOptions(EditorInfo.IME_FLAG_NO_FULLSCREEN);
+        //隐藏光标
+        editText.setCursorVisible(false);
+        //最大输入长度
+        editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(vCodeLength)});
+        //输入类型为数字
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        editText.setTextSize(0);
+        editText.setBackgroundResource(0);
 
-    editText.addTextChangedListener(new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        }
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            if (s != null && !TextUtils.isEmpty(s.toString())) {
-                //有验证码的情况
-                inputData = s.toString();
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && !TextUtils.isEmpty(s.toString())) {
+                    //有验证码的情况
+                    inputData = s.toString();
 
-                //如果是最后一位验证码，焦点在最后一个，否者在下一位
-                if (inputData.length() == vCodeLength) {
-                    tvSetFocus(vCodeLength - 1);
+                    //如果是最后一位验证码，焦点在最后一个，否者在下一位
+                    if (inputData.length() == vCodeLength) {
+                        tvSetFocus(vCodeLength - 1);
+                    } else {
+                        tvSetFocus(inputData.length());
+                    }
+
+                    //给textView设置数据
+                    for (int i = 0; i < inputData.length(); i++) {
+                        tvList.get(i).setText(inputData.substring(i, i + 1));
+                    }
+                    for (int i = inputData.length(); i < vCodeLength; i++) {
+                        tvList.get(i).setText("");
+                    }
                 } else {
-                    tvSetFocus(inputData.length());
-                }
-
-                //给textView设置数据
-                for (int i = 0; i < inputData.length(); i++) {
-                    tvList.get(i).setText(inputData.substring(i, i + 1));
-                }
-                for (int i = inputData.length(); i < vCodeLength; i++) {
-                    tvList.get(i).setText("");
-                }
-            } else {
-                //一位验证码都没有的情况
-                tvSetFocus(0);
-                for (int i = 0; i < vCodeLength; i++) {
-                    tvList.get(i).setText("");
+                    //一位验证码都没有的情况
+                    tvSetFocus(0);
+                    for (int i = 0; i < vCodeLength; i++) {
+                        tvList.get(i).setText("");
+                    }
                 }
             }
-        }
 
-        @Override
-        public void afterTextChanged(Editable s) {
-            if (onVerificationCodeCompleteListener != null) {
-                if (s.length() == vCodeLength) {
-                    onVerificationCodeCompleteListener.verificationCodeComplete(s.toString());
-                } else {
-                    onVerificationCodeCompleteListener.verificationCodeIncomplete(s.toString());
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (onVerificationCodeCompleteListener != null) {
+                    if (s.length() == vCodeLength) {
+                        onVerificationCodeCompleteListener.verificationCodeComplete(s.toString());
+                    } else {
+                        onVerificationCodeCompleteListener.verificationCodeIncomplete(s.toString());
+                    }
                 }
             }
-        }
-    });
-}
+        });
+    }
 
     /**
      * 设置TextView
@@ -187,32 +187,32 @@ private void initEditText() {
 
             //textView.setLayoutParams(layoutParams);
             textView.setBackgroundResource(tvBgNormal);
-            if (isBold){
+            if (isBold) {
                 textView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
             }
             textView.setGravity(Gravity.CENTER);
             //注意单位
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,tvTextSize);
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, tvTextSize);
             textView.setTextColor(tvTextColor);
 
             tvList.add(textView);
         }
     }
 
-/**
- * 假装获取焦点
- */
-private void tvSetFocus(int index) {
-    tvSetFocus(tvList.get(index));
-}
-
-private void tvSetFocus(TextView textView) {
-    for (int i = 0; i < vCodeLength; i++) {
-        tvList.get(i).setBackgroundResource(tvBgNormal);
+    /**
+     * 假装获取焦点
+     */
+    private void tvSetFocus(int index) {
+        tvSetFocus(tvList.get(index));
     }
-    //重新获取焦点
-    textView.setBackgroundResource(tvBgFocus);
-}
+
+    private void tvSetFocus(TextView textView) {
+        for (int i = 0; i < vCodeLength; i++) {
+            tvList.get(i).setBackgroundResource(tvBgNormal);
+        }
+        //重新获取焦点
+        textView.setBackgroundResource(tvBgFocus);
+    }
 
     public int dp2px(final float dpValue) {
         final float scale = getContext().getResources().getDisplayMetrics().density;
@@ -234,20 +234,20 @@ private void tvSetFocus(TextView textView) {
         return (int) (pxValue / fontScale + 0.5f);
     }
 
-/**
- * 输入完成回调接口
- */
-public interface OnVerificationCodeCompleteListener {
-    //完成输入
-    void verificationCodeComplete(String verificationCode);
+    /**
+     * 输入完成回调接口
+     */
+    public interface OnVerificationCodeCompleteListener {
+        //完成输入
+        void verificationCodeComplete(String verificationCode);
 
-    //未完成输入
-    void verificationCodeIncomplete(String verificationCode);
-}
+        //未完成输入
+        void verificationCodeIncomplete(String verificationCode);
+    }
 
-public void setOnVerificationCodeCompleteListener(OnVerificationCodeCompleteListener onVerificationCodeCompleteListener) {
-    VerificationCodeView.onVerificationCodeCompleteListener = onVerificationCodeCompleteListener;
-}
+    public void setOnVerificationCodeCompleteListener(OnVerificationCodeCompleteListener onVerificationCodeCompleteListener) {
+        VerificationCodeView.onVerificationCodeCompleteListener = onVerificationCodeCompleteListener;
+    }
 
 
     /**
